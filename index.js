b6836be6b067a57720e6a8a2d6b52be647cc1483
Mathias Lundell <lundell.mathias@gmail.com>
2025-01-10 11:47:51 +0100

    refactor: log time for resolving

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 'use strict'
 const { Resolver } = require('node:dns/promises')
+const { performance } = require('node:perf_hooks')
 const { callbackify } = require('node:util')
 
 module.exports = function createSrvLookup ({ resolver = new Resolver(), logger = null } = {}) {
   return callbackify(async (origin, opts) => {
+    let start
+    if (logger) {
+      start = performance.now()
+    }
     const srvs = await resolver.resolveSrv(origin.hostname)
     if (srvs.length === 0) {
       logger?.debug('%s resolved no srvs', origin.hostname)
@@ -32,7 +37,10 @@ module.exports = function createSrvLookup ({ resolver = new Resolver(), logger =
       })
     )
 
-    logger?.debug('%s resolved srvs %o\naddresses: %o', origin.hostname, srvs, addresses)
+    if (logger) {
+      const duration = performance.now() - start
+      logger.debug(`${origin.hostname} resolved (${duration.toFixed(3)} ms) srvs %o\naddresses: %o`, srvs, addresses)
+    }
 
     if (addresses.length === 0 && errors.length > 0) {
       throw new AggregateError(errors)
